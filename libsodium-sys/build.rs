67e4190cb75b40ee83d59b9d8689bcf199234993
cssivision <cssivision@gmail.com>
2017-07-26 16:57:56 +0800

    rerun build.rs when env variables change

--- libsodium-sys/build.rs
+++ libsodium-sys/build.rs
@@ -8,7 +8,7 @@ fn main() {
 
     if let Ok(lib_dir) = env::var("SODIUM_LIB_DIR") {
 
-        println!("cargo:rustc-link-search=native={}/lib", lib_dir);
+        println!("cargo:rustc-link-search=native={}", lib_dir);
 
         let mode = match env::var_os("SODIUM_STATIC") {
             Some(_) => "static",
